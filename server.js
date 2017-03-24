f88a5725c01f904ed688120beaf81df9aa4a85a1
[Resire] <resire.creates@gmail.com>
2017-03-23 21:25:09 -0500

    Added enviroment checking for ort assignment

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 "use strict";
 
+// Package dependancies
 const express = require('express');
+
+// Modules
+const opt = require('./config.js');
 const uPath = require('./modules/unique-path');
 
-const app = express();
+// Check if on a local environment
+let local = process.env.NODE_ENV || true;;
+let accessPort = local ? 3000 : process.env.PORT;
 
-app.get("/", function (req, res) {
+const server = express();
+
+server.get("/", function (req, res) {
   let shortURL = `https://${req.headers.host}/${uPath(6)}`;
-  
   res.send(shortURL);
 });
 
-var listener = app.listen(process.env.PORT, function () {
+var listener = server.listen(accessPort, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
