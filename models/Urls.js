274b5e1971d1d312e22ba1ef6d5a72497f4fa244
[Resire] <resire.creates@gmail.com>
2017-03-26 20:16:16 -0500

    Model completed

--- models/Urls.js
+++ models/Urls.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// Package Dependancies
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  original_url: String,
+  unique-path: String
+});
+
+const Url = mongoose.model('Url', schema);
